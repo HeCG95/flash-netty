67c011232a7600729c51f1e7935e3d9fef3f5059
HeCG95 <jacktanger@163.com>
2023-02-13 14:28:54 +0800

    [M] 4.服务端启动流程 2023-2-13 14:28:52

--- src/main/java/the/flash/NettyServer.java
+++ src/main/java/the/flash/NettyServer.java
@@ -13,20 +13,40 @@ public class NettyServer {
     private static final int BEGIN_PORT = 8000;
 
     public static void main(String[] args) {
-        NioEventLoopGroup boosGroup = new NioEventLoopGroup();
-        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        // 1.线程模型
+        NioEventLoopGroup boosGroup = new NioEventLoopGroup();// 监听端口 - 来 accept 新连接的线程组
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();// 处理每一条连接的数据读写的线程组
 
         final ServerBootstrap serverBootstrap = new ServerBootstrap();
         final AttributeKey<Object> clientKey = AttributeKey.newInstance("clientKey");
+        final AttributeKey<Object> serverKey = AttributeKey.newInstance("serverName");
+
         serverBootstrap
                 .group(boosGroup, workerGroup)
+                // 2. IO 模型
                 .channel(NioServerSocketChannel.class)
-                .attr(AttributeKey.newInstance("serverName"), "nettyServer")
+
+                .attr(serverKey, "nettyServer")
                 .childAttr(clientKey, "clientValue")
+
+                // 初始化服务端可连接队列
                 .option(ChannelOption.SO_BACKLOG, 1024)
-                .childOption(ChannelOption.SO_KEEPALIVE, true)
-                .childOption(ChannelOption.TCP_NODELAY, true)
-                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+
+                // 给每条连接设置一些TCP底层相关的属性
+                .childOption(ChannelOption.SO_KEEPALIVE, true)// TCP底层心跳机制
+                .childOption(ChannelOption.TCP_NODELAY, true)// 减少发送次数
+
+                .handler(new ChannelInitializer<NioServerSocketChannel>() {// 在服务端启动过程中的一些逻辑
+                    @Override
+                    protected void initChannel(NioServerSocketChannel ch) throws Exception {
+                        System.out.println("服务端启动中 - attr[serverName] "+ch.attr(serverKey).get());
+                    }
+                })
+
+
+                // 3.连接读写处理逻辑
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {// 处理新连接数据的读写处理逻辑
                     protected void initChannel(NioSocketChannel ch) {
                         System.out.println(ch.attr(clientKey).get());
                     }
@@ -34,6 +54,7 @@ public class NettyServer {
 
 
         bind(serverBootstrap, BEGIN_PORT);
+        System.out.println(">>>>>>>>>>>... ");
     }
 
     private static void bind(final ServerBootstrap serverBootstrap, final int port) {
