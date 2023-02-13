3ee4552d636cd7b123899f6431b2e8e584266ef9
HeCG95 <jacktanger@163.com>
2023-02-13 14:40:17 +0800

    [M] 5.客户端启动流程 2023-2-13 14:40:13

--- src/main/java/the/flash/NettyClient.java
+++ src/main/java/the/flash/NettyClient.java
@@ -19,24 +19,32 @@ public class NettyClient {
 
 
     public static void main(String[] args) {
+
+        // 线程模型
         NioEventLoopGroup workerGroup = new NioEventLoopGroup();
 
         Bootstrap bootstrap = new Bootstrap();
+        AttributeKey<Object> clientKey = AttributeKey.newInstance("clientName");
+
         bootstrap
                 // 1.指定线程模型
                 .group(workerGroup)
-                // 2.指定 IO 类型为 NIO
+                // 2.指定 IO 类型为 NIO - IO 模型
                 .channel(NioSocketChannel.class)
+
                 // 绑定自定义属性到 channel
-                .attr(AttributeKey.newInstance("clientName"), "nettyClient")
+                .attr(clientKey, "nettyClient")
+
                 // 设置TCP底层属性
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)// 连接的超时时间
                 .option(ChannelOption.SO_KEEPALIVE, true)
                 .option(ChannelOption.TCP_NODELAY, true)
+
                 // 3.IO 处理逻辑
                 .handler(new ChannelInitializer<SocketChannel>() {
                     @Override
                     public void initChannel(SocketChannel ch) {
+                        System.out.println("Client key value: "+ch.attr(clientKey).get());
                     }
                 });
 
@@ -45,20 +53,27 @@ public class NettyClient {
     }
 
     private static void connect(Bootstrap bootstrap, String host, int port, int retry) {
+
         bootstrap.connect(host, port).addListener(future -> {
             if (future.isSuccess()) {
                 System.out.println("连接成功!");
             } else if (retry == 0) {
                 System.err.println("重试次数已用完，放弃连接！");
-            } else {
+            } else {// 指数退避重连逻辑
+
                 // 第几次重连
                 int order = (MAX_RETRY - retry) + 1;
                 // 本次重连的间隔
                 int delay = 1 << order;
+                System.out.println("delay times: "+delay);
                 System.err.println(new Date() + ": 连接失败，第" + order + "次重连……");
-                bootstrap.config().group().schedule(() -> connect(bootstrap, host, port, retry - 1), delay, TimeUnit
-                        .SECONDS);
+
+                // 定时器
+                bootstrap.config().group()
+                        .schedule(() -> connect(bootstrap, host, port, retry - 1),
+                                delay, TimeUnit.SECONDS);
             }
         });
+
     }
 }
